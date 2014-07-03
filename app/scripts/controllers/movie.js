3558e295da493385f194ebe83c78f2f665011e2e
Jeremy Parmenter <shabado9889@gmail.com>
2014-07-02 23:22:27 -0700

    Added genres and runtime to movie page

--- app/scripts/controllers/movie.js
+++ app/scripts/controllers/movie.js
@@ -3,7 +3,18 @@
 angular.module('movieTracker')
   .controller('MovieCtrl', ['$scope', '$routeParams', 'movieFactory', 'creditsFactory', function ($scope, $routeParams, movieFactory, creditsFactory) {
     movieFactory.getMovie($routeParams.id).success(function(movieResponse) {
-        $scope.movie = movieResponse;
+        var movie = movieResponse;
+        $scope.title = movie.original_title;
+        $scope.releaseDate = movie.release_date;
+        $scope.overview = movie.overview;
+        $scope.posterImg = movie.poster_path;
+        $scope.runtime = movie.runtime;
+
+        var genreString = '';
+        for (var i = 0; i < movie.genres.length; i++) {
+          genreString += i === movie.genres.length - 1 ? movie.genres[i].name : movie.genres[i].name + ', '
+        }
+        $scope.genres = genreString;
       });
 
     creditsFactory.getCredits($routeParams.id).success(function(creditsResponse) {
